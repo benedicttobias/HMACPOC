152bc28cf43a1ce92ded2ba6e997157c3ab52c99
Benedict Tobias <benedict.tobias@hotmail.com>
2017-07-31 22:46:19 -0500

    Inital commit. need to build HMAC Auth Filter.

---
+++ HMACPOC/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HMACPOC {
+	public partial class _Default : Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			string appID = "4d53bce03ec34c0a911182d4c228ee6c";
+			string appKey = "A93reRTUJHsCuQSHR+L3GxqOJyDmQpCgps102ciuabc=";
+
+
+		}
+	}
+}
