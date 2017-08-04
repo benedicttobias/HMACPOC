9818e7212760f02226de2d163801b5e3d87e53ab
Ben Tobias <benedict.tobias@hotmail.com>
2017-08-04 13:19:19 -0500

    localhost api

---
+++ myAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using myAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace myAPI.Controllers {
+	public class ProductsController : ApiController {
+		Product[] productList = new Product[]
+		{
+			new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+			new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+			new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+		};
+
+		[AcceptVerbs("GET", "HEAD")]
+		public IEnumerable<Product> products() {
+			return productList;
+		}
+
+		[AcceptVerbs("GET", "HEAD")]
+		public IHttpActionResult product(int id) {
+			var product = productList.FirstOrDefault((p) => p.Id == id);
+			if (product == null) {
+				return NotFound();
+			}
+			return Ok(product);
+		}
+	}
+}
---
+++ myAPI/Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myAPI.Models {
+	public class Schedule {
+		public string primary_description { get; set; }
+		public string secondary_description { get; set; }
+		public string tertiary_description { get; set; }
+		public string start_datetime { get; set; }
+		public string end_datetime { get; set; }
+		public string lecturer_name { get; set; }
+		public string lecturer_email { get; set; }
+		public string location_name { get; set; }
+	}
+}
